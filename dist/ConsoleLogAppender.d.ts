be9fd08076b4241e37af64434a70c9b9c8454b45
Mihail Akimenko <mihails.akimenko@gmail.com>
2017-08-15 09:32:47 +0300

    colors

--- dist/ConsoleLogAppender.d.ts
+++ dist/ConsoleLogAppender.d.ts
@@ -1,5 +1,28 @@
 import { LogAppender } from "./LogAppender";
 import { LogLevel } from "./LogLevel";
 export declare class ConsoleLogAppender implements LogAppender {
+    Reset: string;
+    Bright: string;
+    Dim: string;
+    Underscore: string;
+    Blink: string;
+    Reverse: string;
+    Hidden: string;
+    FgBlack: string;
+    FgRed: string;
+    FgGreen: string;
+    FgYellow: string;
+    FgBlue: string;
+    FgMagenta: string;
+    FgCyan: string;
+    FgWhite: string;
+    BgBlack: string;
+    BgRed: string;
+    BgGreen: string;
+    BgYellow: string;
+    BgBlue: string;
+    BgMagenta: string;
+    BgCyan: string;
+    BgWhite: string;
     append(logLevel: LogLevel, name: string, msg: string): void;
 }
--- dist/ConsoleLogAppender.js
+++ dist/ConsoleLogAppender.js
@@ -2,15 +2,43 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const LogLevel_1 = require("./LogLevel");
 class ConsoleLogAppender {
+    constructor() {
+        this.Reset = "\x1b[0m";
+        this.Bright = "\x1b[1m";
+        this.Dim = "\x1b[2m";
+        this.Underscore = "\x1b[4m";
+        this.Blink = "\x1b[5m";
+        this.Reverse = "\x1b[7m";
+        this.Hidden = "\x1b[8m";
+        this.FgBlack = "\x1b[30m";
+        this.FgRed = "\x1b[31m";
+        this.FgGreen = "\x1b[32m";
+        this.FgYellow = "\x1b[33m";
+        this.FgBlue = "\x1b[34m";
+        this.FgMagenta = "\x1b[35m";
+        this.FgCyan = "\x1b[36m";
+        this.FgWhite = "\x1b[37m";
+        this.BgBlack = "\x1b[40m";
+        this.BgRed = "\x1b[41m";
+        this.BgGreen = "\x1b[42m";
+        this.BgYellow = "\x1b[43m";
+        this.BgBlue = "\x1b[44m";
+        this.BgMagenta = "\x1b[45m";
+        this.BgCyan = "\x1b[46m";
+        this.BgWhite = "\x1b[47m";
+    }
     append(logLevel, name, msg) {
         // TODO: make it configurable
         var now = new Date();
-        console.log(("  " + now.toLocaleString()).slice(-19)
+        console.log(this.FgYellow
+            + ("  " + now.toLocaleString()).slice(-19)
             + ": "
             + ("     " + LogLevel_1.LogLevel[logLevel]).slice(-5)
             + ": "
             + (name + "                                  ").substring(0, 25)
-            + ": " + msg);
+            + ": "
+            + this.Reset
+            + msg);
     }
 }
 exports.ConsoleLogAppender = ConsoleLogAppender;
